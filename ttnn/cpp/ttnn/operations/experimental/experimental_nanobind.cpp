bad4a7886bbd4b3e1022ae88359f6f0b0d9dad3f
Pranav Korhale <pkorhale@tenstorrent.com>
2026-05-08 21:52:24 +0530

    fix nanobind

--- ttnn/cpp/ttnn/operations/experimental/experimental_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/experimental/experimental_nanobind.cpp
@@ -4,6 +4,8 @@
 
 #include "experimental_nanobind.hpp"
 
+#include <cstdio>
+
 #include <nanobind/nanobind.h>
 
 #include "ttnn/operations/experimental/adaptive_pool/adaptive_pools_nanobind.hpp"
@@ -116,7 +118,9 @@ void py_module(nb::module_& mod) {
     matmul::detail::bind_group_attn_matmul(mod);
 
     plusone::detail::bind_experimental_plusone_operation(mod);
+    std::fprintf(stderr, "[experimental_nanobind] BEFORE fft binding\n");
     fft::detail::bind_experimental_fft_operation(mod);
+    std::fprintf(stderr, "[experimental_nanobind] AFTER fft binding\n");
     dropout::detail::bind_experimental_dropout_operation(mod);
     reshape::detail::bind_view(mod);
 
